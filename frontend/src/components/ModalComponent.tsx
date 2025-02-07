40883aaa0d371a611a8f7abc3a061cfb38463e57
Brayan Ceron <brayan0np@gmail.com>
2025-02-07 11:48:14 -0500

    update ModalComponent

--- frontend/src/components/ModalComponent.tsx
+++ frontend/src/components/ModalComponent.tsx
@@ -1,40 +1,47 @@
 import { HSOverlay } from "flyonui/flyonui"
 
-export function ModalComponent({ title = "Title",  message, id ,  size= '' } : {title? : string, message : string, id :string, size? :string}) {
-    size = `modal-dialog${size? '-'+size :""}`
-    
+type ModalProps = { title?: string, message: string, id: string, size?: string, vertical?: string, horizontal?: string }
+
+export function ModalComponent({ title = "Title", message, id, size = '', vertical = 'middle', horizontal = 'center' }: ModalProps) {
+
+    size = ['sm', 'lg', 'xlg'].includes(size) ? "modal-dialog-" + size : ''
+    horizontal = ['start', 'end'].includes(horizontal) ? '-' + horizontal : ''
+    vertical = ['top', 'middle', 'bottom'].includes(vertical) ? vertical : 'middle'
+
+    const position = 'modal-'+vertical+horizontal
+
+    console.log(position);
 
     return (
-        <>
-            {/* <button className="btn btn-primary" onClick={abrir}>abrir</button> */}
 
-            <div id={id} className="overlay modal overlay-open:opacity-100 modal-middle hidden" role="dialog" >
-                <div className={"modal-dialog overlay-open:opacity-100 "+size}>
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h3 className="modal-title">{title}</h3>
-                            <button type="button" className="btn btn-text btn-circle btn-sm absolute end-3 top-3" aria-label="Close" data-overlay={"#"+id}>
-                                <span className="icon-[tabler--x] size-4"></span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            {message}
-                        </div>
-                        <div className="modal-footer">
-                            {/* <button type="button" className="btn btn-soft btn-secondary" data-overlay="#middle-center-modal">Close</button> */}
-                            <button type="button" className="btn btn-soft btn-secondary" data-overlay={"#"+id}>Close</button>
-                            <button type="button" className="btn bg-black hover:bg-black text-white">Accept</button>
-                        </div>
+        // <div id={id} className={`overlay modal overlay-open:opacity-100 modal-middle-end hidden`} role="dialog" > 
+        <div id={id} className={"overlay modal overlay-open:opacity-100 " + position + " hidden"} role="dialog" >
+            <div className={"modal-dialog overlay-open:opacity-100 " + size}>
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h3 className="modal-title">{title}</h3>
+                        <button type="button" className="btn btn-text btn-circle btn-sm absolute end-3 top-3" aria-label="Close" data-overlay={"#" + id}>
+                            <span className="icon-[tabler--x] size-4"></span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        {message}
+                    </div>
+                    <div className="modal-footer">
+                        {/* <button type="button" className="btn btn-soft btn-secondary" data-overlay="#middle-center-modal">Close</button> */}
+                        <button type="button" className="btn btn-soft btn-secondary" data-overlay={"#" + id}>Close</button>
+                        <button type="button" className="btn bg-black hover:bg-black text-white">Accept</button>
                     </div>
                 </div>
             </div>
+        </div>
 
-        </>
+
     )
 
 }
 
-export function openModal(event : any, id : string){
+export function openModal(event: any, id: string) {
     event
     HSOverlay.open(`#${id}`)
     // let idd  = document.querySelector('#'+id) 
@@ -42,7 +49,7 @@ export function openModal(event : any, id : string){
     // if(!idd) return
     // modal.open()
 }
-export function closeModal(event : any, id :string){
+export function closeModal(event: any, id: string) {
     event
     HSOverlay.close(`#${id}`)
 }
