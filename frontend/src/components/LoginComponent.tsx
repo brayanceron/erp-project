4f0e8fee0ff012a1289a2d18b8b298376c175a0f
Brayan Ceron <brayan0np@gmail.com>
2025-02-09 14:53:36 -0500

    add LoginComponent

---
+++ frontend/src/components/LoginComponent.tsx
@@ -0,0 +1,71 @@
+import { useForm } from "../hooks/useForm"
+
+/* type loginForm = {
+    email : string,
+    password : string
+} */
+export function LoginComponent() {
+
+    const { formData, onChangeField } = useForm({ email: '', password: '' })
+
+    async function onSubmitForm(event : any){
+        event.preventDefault()
+        // console.log(formData);
+
+        const res = await fetch('http://localhost:5000/api/auth/login',
+            {
+                method : 'POST',
+                headers : { 'Content-Type' : 'application/json'},
+                body : JSON.stringify(formData),
+                credentials : 'include'
+            }
+        )
+        const data = await res.json()
+        console.log(res);
+        console.log(res.formData);
+        console.log(res.headers);
+        console.log(data);
+    }
+
+    return (
+        <>
+            <div className="shadow-md sm:w-full md:w-1/2 max-w-[380px] max-[640px]:p-1 sm:p-3 md:p-4 m-2 ">
+
+                <h1 className="text-4xl text-center font-bold my-8">Login</h1>
+
+                <form action="" onSubmit={onSubmitForm}>
+
+                    <div className="auto mt-4">
+                        <label className="label label-text" htmlFor="phone"> Email </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--mail] text-base-content/80 size-5"></span>
+                            </span>
+                            <input type="text" placeholder="" className="input grow" id="email" name="email" onChange={onChangeField} />
+                        </div>
+                    </div>
+
+                    <div className="auto mt-4">
+                        <label className="label label-text" htmlFor="phone"> Password </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--lock] text-base-content/80 size-5"></span>
+                            </span>
+                            <input type="password" placeholder="" className="input grow" id="password" name="password" onChange={onChangeField} />
+                        </div>
+                    </div>
+
+                    <div className="w-auto mt-16 mb-5">
+                        <button type="submit" className="btn btn-block bg-black text-white hover:bg-black">Login</button>
+                    </div>
+                    <div className="w-auto mb-5 text-center">
+                        <a href="#" className="link link-animated"> Forget my password</a>
+                    </div>
+
+                </form>
+
+
+            </div>
+        </>
+    )
+}
