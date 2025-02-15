48a7f47a6ff020eae8c40c910ee996870d3d0018
Brayan Ceron <brayan0np@gmail.com>
2025-02-15 17:47:39 -0500

    update useFetch

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react"
 
 type Params = {
-    data : any,
-    isLoading : boolean,
-    error : any
-    // error : Error | null
+    data: any, // data : {} | null | [],
+    isLoading: boolean,
+    error: any // error : Error | null
 }
 
-export function useFetch(url : string){
-
-    // const [req, setReq] = useState({
+export function useFetch(url: string) {
     const [req, setReq] = useState<Params>({
         data : null,
         isLoading : true,
@@ -18,27 +15,30 @@ export function useFetch(url : string){
 
     // const {data, isLoading, error} = req;
 
-    async function getData(){
+    async function getData() {
         try {
             const res = await fetch(url)
             const data = await res.json()
             setReq({
                 data,
                 isLoading: false,
-                error : null
+                error: res.ok ? null : Error(data)
             })
         } catch (error) {
             setReq({
-                data : null,
-                isLoading : false,
-                error : error
+                data: null,
+                isLoading: false,
+                error: error
             })
         }
-        
+
     }
 
-    useEffect(()=>{
-        if(!url) return
+    useEffect(() => {
+        if (!url) {
+            setReq({ data: null, isLoading: false, error: Error("Invalid Url") })
+            return
+        }
         getData()
     }, [url])
 
