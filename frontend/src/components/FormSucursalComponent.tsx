ac4d8e4103b5a1725afef6de6d038d014d26db90
Brayan Ceron <brayan0np@gmail.com>
2025-03-06 13:52:54 -0500

    add usePost, postcallback & navigate to FormComponent

--- frontend/src/components/FormSucursalComponent.tsx
+++ frontend/src/components/FormSucursalComponent.tsx
@@ -2,6 +2,8 @@ import { useState } from "react"
 import { useForm } from "../hooks/useForm"
 import { LocationComponent } from "./Location/LocationComponent"
 import { ModalComponent, openModal } from "./ModalComponent"
+import { useNavigate } from 'react-router'
+import { usePost, paramsPost, Method } from "../hooks/usePost"
 
 type FormSucursal = {
     name?: string,
@@ -11,123 +13,102 @@ type FormSucursal = {
     address?: string,
     description?: string
 }
-export function FormSucursalComponent({ defaultValues, title = "Registrar" }: { defaultValues?: FormSucursal, title?: string }) {
-
+export function FormSucursalComponent({ defaultValues, url, method = Method.POST, }: { defaultValues?: FormSucursal, title?: string, url : string, method? : Method }) {
+    let navigate = useNavigate()
     const { formData, onChangeField, setFields } = useForm({ ...defaultValues })
     const [messageModal, setMessageModal] = useState('')
 
     function getData(country: any, city: any) {
         const CountryAndCity = [
-            { field: 'city', value: city },
-            { field: 'country', value: country },
+            { field: 'city', value: city.id },
+            { field: 'country', value: country.id },
         ]
         setFields(CountryAndCity)
     }
 
+    function postCallback({ error, data, body }: paramsPost) {
+        setMessageModal(error ? error.message : data.message)
+        if (!error) { return navigate(`/sucursal/get/${method == Method.POST? data.id : method == Method.PUT ? body.id : ''}`) }
+        openModal(null, 'modalFormSucursal')
+    }
+
+    const { sendReq, isLoading } = usePost(url, formData, method, postCallback)
     async function onSubmitForm(event: any) {
         event.preventDefault()
-        const dataSend = { ...formData, country: formData.country.id, city: formData.city.id }
-        // console.log(formData);
-        // console.log(dataSend);
-
-        try {
-            // const res = await fetch('http://localhost:8081/api/sucursal/', {
-            const res = await fetch('http://localhost:5000/api/sucursal/', { // const res = await fetch('/api/sucursal/', {
-                body: JSON.stringify(dataSend),
-                method: 'post',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-            })
-            // console.log(res);
-            const resdata = await res.json()
-            console.log(resdata);
-            if(res.status == 200){
-                console.log("Oook");
-                setMessageModal(resdata.message)
-                openModal(event,'modalFormSucursal')
-            }
-            else{
-                console.log("BAD");
-                setMessageModal(resdata.message)
-                openModal(event, 'modalFormSucursal')
-            }
-        } catch (error) {
-            console.log("ERRor");
-            console.log(error);
-
-        }
-
+        await sendReq()
     }
 
     return (
-        <form action="" onSubmit={onSubmitForm}>
+        <>
+            <ModalComponent id="modalFormSucursal" message={messageModal} vertical="middle" />
             <div className="container card p-7 m-3 sm:w-full md:w-[55%] lg:w-[35%] xl:w-[28%]">
+                <form action="" onSubmit={onSubmitForm}>
 
-                <h3 className="text-2xl font-bold text-center">{title} </h3>
+                    <h3 className="text-2xl font-bold text-center">{method == Method.PUT? "Actualizar" : method == Method.POST? "Registrar" : ''}</h3>
 
-                <div className="w-auto">
-                    {/* <div className=" inline-block md:w-1/2"> */}
-                    <label className="label label-text" htmlFor="names"> Name </label>
-                    <div className="input-group w-auto">
-                        <span className="input-group-text">
-                            <span className="icon-[tabler--text-spellcheck] text-base-content/80 size-5"></span>
-                        </span>
-                        <input type="text" className="input grow" value={formData.name} onChange={onChangeField} id="name" name="name" />
+                    <div className="w-auto">
+                        {/* <div className=" inline-block md:w-1/2"> */}
+                        <label className="label label-text" htmlFor="names"> Name </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--text-spellcheck] text-base-content/80 size-5"></span>
+                            </span>
+                            <input type="text" className="input grow" value={formData.name} onChange={onChangeField} id="name" name="name" />
+                        </div>
                     </div>
-                </div>
 
 
-                {
-                    (defaultValues?.country && defaultValues?.city) ?
-                        <LocationComponent
-                            getData={getData}
-                            countryDefault={defaultValues.country}
-                            cityDefault={defaultValues.city} />
-                        :
-                        <LocationComponent getData={getData} />
-                }
+                    {
+                        (defaultValues?.country && defaultValues?.city) ?
+                            <LocationComponent
+                                getData={getData}
+                                countryDefault={defaultValues.country}
+                                cityDefault={defaultValues.city} />
+                            :
+                            <LocationComponent getData={getData} />
+                    }
 
-                <div className="w-auto">
-                    <label className="label label-text" htmlFor="phone"> Phone </label>
-                    <div className="input-group w-auto">
-                        <span className="input-group-text">
-                            <span className="icon-[tabler--phone-filled] text-base-content/80 size-5"></span>
-                        </span>
-                        <input type="number" placeholder="" className="input grow" value={formData.phone} onChange={onChangeField} id="phone" name="phone" />
+                    <div className="w-auto">
+                        <label className="label label-text" htmlFor="phone"> Phone </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--phone-filled] text-base-content/80 size-5"></span>
+                            </span>
+                            <input type="number" placeholder="" className="input grow" value={formData.phone} onChange={onChangeField} id="phone" name="phone" />
+                        </div>
                     </div>
-                </div>
 
-                <div className="auto">
-                    <label className="label label-text" htmlFor="phone"> Address </label>
-                    <div className="input-group w-auto">
-                        <span className="input-group-text">
-                            <span className="icon-[tabler--location] text-base-content/80 size-5"></span>
-                        </span>
-                        <input type="text" placeholder="" className="input grow" value={formData.address} onChange={onChangeField} id="address" name="address" />
+                    <div className="auto">
+                        <label className="label label-text" htmlFor="phone"> Address </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--location] text-base-content/80 size-5"></span>
+                            </span>
+                            <input type="text" placeholder="" className="input grow" value={formData.address} onChange={onChangeField} id="address" name="address" />
+                        </div>
                     </div>
-                </div>
 
-                <div className="auto">
-                    <label className="label label-text" htmlFor="phone"> Description </label>
-                    <div className="input-group w-auto">
-                        <span className="input-group-text">
-                            <span className="icon-[tabler--baseline-density-small] text-base-content/80 size-5"></span>
-                        </span>
-                        {/* <input type="number" placeholder="" className="input grow"   id="description" name="description"/> */}
-                        {/* <input type="text" placeholder="" className="input grow" /> */}
-                        <textarea className="textarea max-w-sm" value={formData.description} onChange={onChangeField} id="description" name="description" aria-label="Textarea" ></textarea>
+                    <div className="auto">
+                        <label className="label label-text" htmlFor="phone"> Description </label>
+                        <div className="input-group w-auto">
+                            <span className="input-group-text">
+                                <span className="icon-[tabler--baseline-density-small] text-base-content/80 size-5"></span>
+                            </span>
+                            {/* <input type="number" placeholder="" className="input grow"   id="description" name="description"/> */}
+                            {/* <input type="text" placeholder="" className="input grow" /> */}
+                            <textarea className="textarea max-w-sm" value={formData.description} onChange={onChangeField} id="description" name="description" aria-label="Textarea" ></textarea>
+                        </div>
                     </div>
-                </div>
 
-                <div className="w-auto my-3">
-                    <button type="submit" className="btn btn-block bg-black text-white hover:bg-black">{title}</button>
-                </div>
+                    <div className="w-auto my-3">
+                        <button type="submit" className={`btn btn-block bg-black text-white hover:bg-black ${isLoading && "btn-disabled"}`}>
+                            {isLoading && <span className="loading loading-spinner loading-sm"></span>}
+                            <span>{method == Method.PUT? "Actualizar" : method == Method.POST? "Registrar" : ''}</span>
+                        </button>
+                    </div>
 
+                </form>
             </div>
-            <ModalComponent id="modalFormSucursal" message={messageModal} vertical="middle"/>
-        </form>
-
+        </>
     )
 }
