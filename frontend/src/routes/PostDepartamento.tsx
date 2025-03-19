0186a6d983a1524b8913d650f0749cba491dbb4f
Brayan Ceron <brayan0np@gmail.com>
2025-03-19 11:26:13 -0500

    update : get idSucursal from url

--- frontend/src/routes/PostDepartamento.tsx
+++ frontend/src/routes/PostDepartamento.tsx
@@ -1,11 +1,14 @@
 import { FormDepartmentComponent } from "../components/FormDepartmentComponent";
+import { useSearchParams } from 'react-router'
 
 export function PostDepartamento() {
+    const [searchParams] = useSearchParams();
+    const idSucursal = searchParams.get('idSucursal') || ''
     return (
         <>
             <div>
                 <div className="w-full h-full flex justify-center items-center">
-                    <FormDepartmentComponent idSucursal="cfc44aa0-fb50-4c13-bf82-d8ae7c3ea1ea" url="http://localhost:5000/api/departamento" />
+                    <FormDepartmentComponent idSucursal={idSucursal} url="http://localhost:5000/api/departamento" />
                 </div>
             </div>
         </>
