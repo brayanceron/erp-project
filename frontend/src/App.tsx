c100319de66e463ee34adbee03b526b9c3c213da
Brayan Ceron <brayan0np@gmail.com>
2025-07-28 21:38:53 -0500

    update routes on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,132 +19,96 @@ import PostActividad from "./routes/activity/PostActividad"
 import GetActividades from "./routes/activity/GetActividades"
 import GetByDate from "./routes/activity/GetByDate"
 // import { ModalComponent, openModal } from "./components/ModalComponent"
+import UserProvider from "./auth/UserProvider"
+import { ProtectedRoute } from "./auth/ProtectedRoute"
+import { ProtectedLogin } from "./auth/ProtectedLogin"
+import { useEffect } from "react"
+
+// -------------|| Esto arregla el maldito problema ||---------------
+import { useLocation } from "react-router";
+import { IStaticMethods } from 'flyonui/flyonui';
+import PostRol from "./routes/rol/PostRol"
+import GetRoles from "./routes/rol/GetRoles"
+import GetRol from "./routes/rol/GetRol"
+import PutRol from "./routes/rol/PutRol"
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
+// -------------|| Esto arregla el maldito problema ||---------------
 
 function App() {
 
+  // -------------|| Esto arregla el maldito problema ||---------------
+  const location = useLocation();
+  useEffect(() => {
+    const loadFlyonui = async () => {
+      await import('flyonui/flyonui');
+      window.HSStaticMethods.autoInit();
+    };
+    loadFlyonui();
+  }, [location.pathname]);
+  // -------------|| Esto arregla el maldito problema ||---------------
+
+
   return (
-    <>
+    <UserProvider>
       <Routes>
 
-        <Route element={<LayoutDashboard />}>
+        <Route element={<ProtectedRoute> <LayoutDashboard /> </ProtectedRoute>}> {/* <Route element={<LayoutDashboard />}> */}
 
-          <Route path="/" element={<h1>Root page</h1>}></Route>
+          <Route path="/" element={<h1>root route</h1>}></Route>
 
           <Route path="sucursal">
-            <Route path="get" element={<GetSucursales />}></Route>
-            <Route path="get/:id" element={<GetSucursal />}></Route>
-            <Route path="post" element={<PostSucursal />}></Route>
-            <Route path="put/:id" element={<PutSucursal />}></Route>
-            <Route path="search" element={<SearchSucursal />}></Route>
+            {/* <Route path="get" element={<GetSucursales />} /> */}
+            <Route path="get" element={<GetSucursal />} />
+            <Route path="get/:id" element={<GetSucursal />} />
+            <Route path="post" element={<PostSucursal />} />
+            <Route path="put/:id" element={<PutSucursal />} />
+            <Route path="search" element={<SearchSucursal />} />
+            <Route path="explore" element={<GetSucursales />} />
           </Route>
 
-          {/* <Route path="departamento" element={<GetDepartamento />}> */}
-          <Route path="departamento" >
-            <Route path="get/:id" element={<GetDepartamento />}></Route>
-            <Route path="post" element={<PostDepartamento />}></Route>
-            <Route path="put/:id" element={<PutDepartamento />}></Route>
+          <Route path="departamento" >{/* <Route path="departamento" element={<GetDepartamento />}> */}
+            <Route path="get/" element={<GetDepartamento />} />
+            <Route path="get/:id" element={<GetDepartamento />} />
+            <Route path="post" element={<PostDepartamento />} />
+            <Route path="put/:id" element={<PutDepartamento />} />
             {/* <Route path="put/:id" element={<FormDepartmentComponent idSucursal="cfc44aa0-fb50-4c13-bf82-d8ae7c3ea1ea" url="http://localhost:5000/api/departamento" />}></Route> */}
           </Route>
 
           <Route path="usuario">
-            {/* <Route path="post" element={<FormUserComponent />}></Route> */}
-
-            <Route path="get/" element={<UserProfile />}></Route> // usuario logueado
-            <Route path="get/:id" element={<UserProfile />}></Route>
-            <Route path="post" element={<PostUser />}></Route>
-            <Route path="put/:id" element={<PutUser/>}></Route>
-            <Route path="search/" element={<SearchUser/>}></Route>
-
+            <Route path="get/" element={<UserProfile />} /> // usuario logeado
+            <Route path="get/:id" element={<UserProfile />} />
+            <Route path="post" element={<PostUser />} />
+            <Route path="put/:id" element={<PutUser />} />
+            <Route path="search/" element={<SearchUser />} />
           </Route>
 
           <Route path="actividad">
-            <Route path="get/" element={<GetActividades/>}></Route>
-            <Route path="get/:id" element={<GetActividad/>}></Route>
-            <Route path="get/by/date" element={<GetByDate/>}></Route>
-            <Route path="post/"   element={<PostActividad/>}></Route> //calendar
+            <Route path="get/" element={<GetActividades />} />
+            <Route path="get/:id" element={<GetActividad />} />
+            <Route path="get/by/date" element={<GetByDate />} />
+            <Route path="post/" element={<PostActividad />} /> // calendar
+          </Route>
+
+          <Route path="rol">
+            <Route path="get/" element={<GetRoles/>} />
+            <Route path="get/:id" element={<GetRol/>} />
+            <Route path="post/" element={<PostRol />} />
+            <Route path="put/:id" element={<PutRol />} />
           </Route>
 
           <Route path="/*" element={<h1>404. Building...</h1>} ></Route>
         </Route>
 
-        <Route path="/login" element={<LoginComponent />}>
-        </Route>
+        <Route path="/login" element={<ProtectedLogin> <LoginComponent /> </ProtectedLogin>} />
 
       </Routes >
 
-    </>
+    </UserProvider>
   )
 }
 
 export default App
-
-
-{/* <GetSucursal /> */ }
-{/* <GetSucursal id="0338e8e9-d6f8-47aa-a661-b2d8263f664f" /> */ }
-
-{/* <GetSucursal id="43f663f7-af4e-4c49-b4ed-0eb61a761b3f"/> */ }
-{/* <GetSucursales/> */ }
-
-{/* <FormSucursalComponent/> */ }
-{/* <FormSucursalComponent
-          defaultValues={{
-            name: "Sucursal Test",
-            phone: "000000000",
-            country: { id: 'ARG', name: 'xxx' },
-            city: { id: '69', name: 'xxx' },
-          }}
-        /> */}
-{/* <FormUserComponent /> */ }
-{/* <FormUserComponent 
-          defaultValues={
-            {
-              names : 'Brayan Daniel', 
-              lastnames: 'Ceron Portilla',
-              phone : '3003436887', 
-              birthday :'04/07/1997', 
-              gender : 'M',
-              dni: '1085335942',
-              email: 'brayan0np@gmail.com',
-              role: 'admin',
-              password: '123456',
-              country: {id:'ARG', name:'xxx'},
-              city:  {id:'68', name:'xxx'},
-            }
-          }
-      /> */}
-
-{/* <UserProfile idUser='8fbb558a-0d76-40fa-84ee-316d5082f34c' /> */ }
-
-
-{/* <h1>App</h1> */ }
-
-
-
-{/* <FormSucursalComponent/> */ }
-{/* <FormSucursalComponent 
-        defaultValues={{
-          name :"Sucursal Test",
-          phone : "000000000",
-          country: {id:'ARG', name:'xxx'},
-          city:  {id:'68', name:'xxx'},
-        }}
-      /> */}
-
-
-
-
-{/* 
-      <button className="btn bg-black" aria-haspopup="dialog" aria-expanded="false" aria-controls="modal01" data-overlay="#modal01">Open</button>
-      <button className="btn btn-primary" onClick={event => openModal(event, "modal01")}>abrir m1</button>
-      <ModalComponent title={"Mi dialog1"} message={"Este es un mensaje de prueba solamente M1"} id="modal01" size="" />
-
-      <button className="btn btn-primary" onClick={event => openModal(event, "modal02")}>abrir m2</button>
-      <ModalComponent title={"Mi dialog2"} message={"Este es un mensaje de prueba solamente M2"} id="modal02" size="lg" vertical="middle" horizontal="start" />
- */}
-
-
-
-
-
-{/* <FormSucursalComponent/> */ }
-{/* <LoginComponent /> */ }
